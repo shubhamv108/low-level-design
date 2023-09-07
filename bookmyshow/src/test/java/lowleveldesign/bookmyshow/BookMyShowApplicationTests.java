7cfe68e817237fb0182601d9ce937972b8e6fe69
Shubham Varshney <16763337+shubhamv108@users.noreply.github.com>
2023-09-07 21:12:31 +0530

    uf

--- bookmyshow/src/test/java/lowleveldesign/bookmyshow/BookMyShowApplicationTests.java
+++ bookmyshow/src/test/java/lowleveldesign/bookmyshow/BookMyShowApplicationTests.java
@@ -3,7 +3,7 @@ package lowleveldesign.bookmyshow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BookMyShowApplicationTests {
 
 	@Test
